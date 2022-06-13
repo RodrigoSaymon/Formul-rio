39df29d8bbab92b42b35a6f8f64659ed54e235d1
Rodrigo Saymon Fragoso Borges <rodrigotbass@gmail.com>
2022-06-13 20:34:35 -0300

    5

--- formulario/lib/main.dart
+++ formulario/lib/main.dart
@@ -11,10 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
+      title: 'Flutter Demo',
       home: UserList(),
     );
   }
